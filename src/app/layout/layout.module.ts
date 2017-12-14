4256b628c63c77b2add0348060fe3da1e4c78e38
yuzhuo li <yuzhuoli@yuzhuos-MacBook-Pro.local>
2017-12-14 14:12:26 +0800

    remove translate modules

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule, Http } from '@angular/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 
-export function HttpLoaderFactory(http:Http){
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 @NgModule({
-  imports: [CommonModule,
-    TranslateModule.forRoot({
-      loader:{
-        provide:TranslateLoader,
-        useFactory:HttpLoaderFactory,
-        deps:[Http]
-      }
-    }
-    )
-  ],
+  imports: [CommonModule],
   providers: [],
   declarations: []
 })
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule, Http } from '@angular/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { ProductRoutingModule, routedComponents } from './product-routing.module';
 
@@ -11,20 +9,10 @@ import { ProductlistComponent } from './productlist/productlist.component';
 import { ProductService } from '../services/product.service';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-export function HttpLoaderFactory(http:Http){
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+
 @NgModule({
   imports: [CommonModule,
-    ProductRoutingModule,
-    TranslateModule.forRoot({
-      loader:{
-        provide:TranslateLoader,
-        useFactory:HttpLoaderFactory,
-        deps:[Http]
-      }
-    }
-    )
+    ProductRoutingModule
   ],
   providers: [ProductService],
   declarations: [routedComponents,
